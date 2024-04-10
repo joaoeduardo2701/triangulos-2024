f7ab539c18f215f6874234150ea77f334fa11ecd
João Eduardo Cardoso Padilha <duducardoso2701@gmail.com>
2024-04-10 17:10:46 -0300

    Código refatorado

--- src/Triangulos.ConsoleApp/Program.cs
+++ src/Triangulos.ConsoleApp/Program.cs
@@ -4,73 +4,18 @@
     {
         static void Main(string[] args)
         {
-            char opcao = 'S';
-            do
+            Triangulo t = new Triangulo(2, 2, 3);
+
+            // Verificando se o triângulo é válido
+            if (t.TrianguloValido())
             {
-                Console.Clear();
-
-                Console.WriteLine("-- Verificação de triângulos --,");
-
-                Console.WriteLine("Digite o primeiro segmento: ");
-                Triangulos.x = double.Parse(Console.ReadLine());
-
-                Console.WriteLine("Digite o segundo segmento: ");
-                Triangulos.y = double.Parse(Console.ReadLine());
-
-                Console.WriteLine("Digite o terceiro segmento: ");
-                Triangulos.z = double.Parse(Console.ReadLine());
-
-
-
-                if (Triangulos.TrianguloPodeSerCriado(Triangulos.x, Triangulos.y, Triangulos.z) == false)
-                {
-                    Console.WriteLine("Por favor digite uma opção válida");
-                }
-
-                Triangulos.TipoTriangulo(Triangulos.x, Triangulos.y, Triangulos.z);
-
-                Console.WriteLine("Deseja continuar (S/N)");
-                opcao = char.Parse(Console.ReadLine());
-
-            } while (opcao != 'N' || opcao != 'n');
-        }
-    }
-
-    public static class Triangulos
-    {
-        public static double x;
-        public static double y;
-        public static double z;
-
-        public static bool TrianguloPodeSerCriado(double x, double y, double z)
-        {
-            if (x < y + z && y < x + z && z < x + y)
-            {
-                return true;
-            } 
+                Console.WriteLine("O triângulo é válido");
+                Console.WriteLine("O triângulo é " + t.ObterTipo());
+            }
             else
             {
-                return false;
+                Console.WriteLine("O triângulo não é válido");
             }
-        }
-
-        public static void TipoTriangulo(double x, double y, double z)
-        {
-            Console.Write("Triângulo ");
-
-            if (x != y && y != z && z != x)
-            {
-                Console.WriteLine("Escaleno");
-            }
-            else if (x == y && y == z && z == x)
-            {
-                Console.WriteLine("Equilátero");
-            }
-            else if (x == y || y == z || z == x)
-            {
-                Console.WriteLine("Isósceles");
-            }
-            
         }
     }
 }
---
+++ src/Triangulos.ConsoleApp/Triangulo.cs
@@ -0,0 +1,49 @@
+﻿namespace Triangulos.ConsoleApp;
+public class Triangulo
+{
+    public double LadoX;
+    public double LadoY;
+    public double LadoZ;
+
+    public Triangulo(double ladoX, double ladoY, double ladoZ)
+    {
+        LadoX = ladoX;
+        LadoY = ladoY;
+        LadoZ = ladoZ;
+    }
+
+    public string ObterTipo()
+    {
+        string tipo = "";
+
+        if (LadoX == LadoY && LadoX == LadoZ && LadoY == LadoZ)
+        {
+            tipo = "Equilátero";
+            return tipo;
+        }
+        else if (LadoX != LadoY && LadoX != LadoZ && LadoY != LadoZ)
+        {
+            tipo = "Escaleno";
+            return tipo;
+        }
+        else if (LadoX == LadoY || LadoX == LadoZ || LadoY == LadoZ)
+        {
+            tipo = "Isósceles";
+            return tipo;
+        }
+
+        return tipo;
+    }
+
+    public bool TrianguloValido()
+    {
+        if (LadoX  > LadoY + LadoZ || LadoY > LadoX + LadoZ || LadoZ > LadoX + LadoY)
+        {
+            return false;
+        }
+        else
+        {
+            return true;
+        }
+    }
+}
